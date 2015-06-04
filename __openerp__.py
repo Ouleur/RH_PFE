5140d2076f4417c9822acc76ca4ba64adcbdd7c1
Ouleur <ouleur000@gmail.com>
2015-06-04 15:08:06 +0000

    Création du module

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name':'Gestion de compte',
+    'version': '0.0.1',
+    'category': 'Generic Modules/Others',
+    'description': """Interface Ressource Humaine""",
+    'author':'SMILE',
+    'depends':['base'],
+    'data':['view/cmpView.xml'],
+    'installable':True,
+    'auto_install':False,
+}
